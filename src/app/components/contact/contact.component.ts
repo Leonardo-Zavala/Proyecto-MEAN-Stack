abb750d99957778cdf20aff532ce2142dcae5ed8
Leonardo-Zavala <leo.zavala90@gmail.com>
2021-01-31 15:08:35 -0300

    version 2

---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+declare var $:any;
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+  public widthSlider: number;
+  public anchuraToSlider: number;
+  public captions: boolean;
+  public autor: any;
+  @ViewChild('textos', {static:true}) textos;
+
+  constructor() {
+    this.captions = true;
+  }
+
+  ngOnInit(){
+    var opcion_clasica = document.querySelector('#texto').innerHTML;
+    // alert(this.textos.nativeElement.textContent);
+  }
+
+  cargarSlider(){
+    this.anchuraToSlider = this.widthSlider;
+  }
+
+  resetearSlider(){
+    this.anchuraToSlider = null;
+    this.widthSlider = null;
+    this.autor = null;
+  }
+
+  getAutor(event){
+    this.autor = event;
+  }
+
+
+
+}
---
+++ src/app/components/slider/slider.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+declare var $:any;
+
+@Component({
+  selector: 'slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.css']
+})
+export class SliderComponent implements OnInit {
+
+	@Input() anchura: number;
+	@Input('etiquetas') captions: boolean;
+	@Output() conseguirAutor = new EventEmitter();
+
+	public autor: any;
+
+  constructor() {
+
+  	this.autor = {
+  		nombre: 'Leonardo Zavala',
+  		website: 'victorroblesweb.es',
+  		youtube: 'Victor Robles WEB'
+  	};
+
+  }
+
+  ngOnInit() {
+  	 	$('#logo').click(function(e){
+  		e.preventDefault();
+  		$('header').css('background', 'green')
+  				   .css('height','50px');	
+  	});
+
+  	$('.galeria').bxSlider({
+    mode: 'fade',
+    captions: this.captions,
+    slideWidth: this.anchura
+  });
+  }
+
+  lanzar(event){
+  	this.conseguirAutor.emit(this.autor);
+  }
+
+}
